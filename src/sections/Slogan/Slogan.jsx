bdcbb9da79aa2c0a046f781cd85e6b4ba60543d2
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-04-08 05:16:12 +0300

    g

--- src/sections/Slogan/Slogan.jsx
+++ src/sections/Slogan/Slogan.jsx
@@ -13,11 +13,11 @@ export default function Slogan() {
   return (
     <>
     <div className="cards text-center mb-5">
-        <div className="card0 left">{card[0]}<p className="card0">{t("")}</p></div>
-        <div className="card1 left">{card[1]} <p className="card1">{t("")}</p></div>
-        <div className="card2 hidden">{card[2]} <p className="card2">{t("")}</p> </div>
-        <div className="card3 hidden">{card[3]} <p className="card3">{t("")}</p> </div>
-        <div className="card4 right">{card[4]} <p className="card4">{t("")}</p> </div>
+        <div className="card0 left">{card[0]}<p className="card0">{t("Slogan-card1")}</p></div>
+        <div className="card1 left">{card[1]} <p className="card1">{t("Slogan-card2")}</p></div>
+        <div className="card2 hidden">{card[2]} <p className="card2">{t("Slogan-card3")}</p> </div>
+        <div className="card3 hidden">{card[3]} <p className="card3">{t("Slogan-card4")}</p> </div>
+        <div className="card4 right">{card[4]} <p className="card4">{t("Slogan-card5")}</p> </div>
       </div>
     
     </>
--- src/sections/Slogan2/Slogan2.jsx
+++ src/sections/Slogan2/Slogan2.jsx
@@ -13,11 +13,11 @@ export default function Sloga2() {
   return (
     <>
     <div className="cards text-center  mb-5">
-        <div className="card0 left">{card[5]}<p className="card5">{t("")}</p></div>
-        <div className="card1 left">{card[6]} <p className="card6">{t("")}</p></div>
-        <div className="card2 hidden">{card[7]} <p className="card7">{t("")}</p> </div>
-        <div className="card3 hidden">{card[8]} <p className="card8">{t("")}</p> </div>
-        <div className="card4 right">{card[9]} <p className="card9">{t("")}</p> </div>
+        <div className="card0 left">{card[5]}<p className="card5">{t("Slogan-card6")}</p></div>
+        <div className="card1 left">{card[6]} <p className="card6">{t("Slogan-card7")}</p></div>
+        <div className="card2 hidden">{card[7]} <p className="card7">{t("Slogan-card8")}</p> </div>
+        <div className="card3 hidden">{card[8]} <p className="card8">{t("Slogan-card9")}</p> </div>
+        <div className="card4 right">{card[9]} <p className="card9">{t("Slogan-card10")}</p> </div>
       </div>
     
     </>
