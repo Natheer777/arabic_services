375aad14ca67689f43e99e9c1e50e9d8552ce1d3
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-04-16 14:47:59 +0300

    p

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -1,12 +1,12 @@
 // Import Swiper React components
 
-import HeaderImg_01 from "../../assets/Icons/img1 1.webp";
-import HeaderImg_02 from "../../assets/Icons/img1 (2).webp";
-import HeaderImg_03 from "../../assets/Icons/img1 (3).webp";
-import HeaderImg_04 from "../../assets/Icons/img1 (5).webp";
-import HeaderImg_05 from "../../assets/Icons/img1 (6).webp";
-import HeaderImg_06 from "../../assets/Icons/img1 (7).webp";
-import HeaderImg_07 from "../../assets/Icons/img1 (8).webp";
+import HeaderImg_01 from "../../assets/photo_2024-04-15_22-41-37.jpg";
+// import HeaderImg_02 from "../../assets/Icons/img1 (2).webp";
+// import HeaderImg_03 from "../../assets/Icons/img1 (3).webp";
+// import HeaderImg_04 from "../../assets/Icons/img1 (5).webp";
+// import HeaderImg_05 from "../../assets/Icons/img1 (6).webp";
+// import HeaderImg_06 from "../../assets/Icons/img1 (7).webp";
+// import HeaderImg_07 from "../../assets/Icons/img1 (8).webp";
 
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -44,12 +44,16 @@ export default function Header() {
               className="mySwiper"
             >
               <SwiperSlide className="img1"><img src={HeaderImg_01} alt="" /></SwiperSlide>
-              <SwiperSlide className="img2"><img src={HeaderImg_02} alt="" /></SwiperSlide>
+              <SwiperSlide className="img1"><img src={HeaderImg_01} alt="" /></SwiperSlide>
+              <SwiperSlide className="img1"><img src={HeaderImg_01} alt="" /></SwiperSlide>
+              <SwiperSlide className="img1"><img src={HeaderImg_01} alt="" /></SwiperSlide>
+              {/* <SwiperSlide className="img2"><img src={HeaderImg_02} alt="" /></SwiperSlide>
               <SwiperSlide className="img3"><img src={HeaderImg_03} alt="" /></SwiperSlide>
               <SwiperSlide className="img4"><img src={HeaderImg_04} alt="" /></SwiperSlide>
               <SwiperSlide className="img5"><img src={HeaderImg_05} alt="" /></SwiperSlide>
               <SwiperSlide className="img6"><img src={HeaderImg_06} alt="" /></SwiperSlide>
-              <SwiperSlide className="img7"><img src={HeaderImg_07} alt="" /></SwiperSlide>            </Swiper>
+              <SwiperSlide className="img7"><img src={HeaderImg_07} alt="" /></SwiperSlide>      */}
+                     </Swiper>
           </div>
           </div>
     </>
